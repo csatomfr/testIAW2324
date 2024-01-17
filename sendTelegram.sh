1e6b87b2db864149eb8a31ade64312e665ab977a
csatomfr <csatomfr@hotmail.com>
2024-01-17 19:56:35 +0100

    Create sendTelegram.sh

---
+++ sendTelegram.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -X POST -H \'Content-Type: application/json\' -d \'{"chat_id": "902285901", "text": "Ha petado el test :( ", "disable_notification": false}\'  https://api.telegram.org/bot6972167273:AAFfUCU_9JQyRVkQSbftk_Sc3FVqSpE_SEg/sendMessage
